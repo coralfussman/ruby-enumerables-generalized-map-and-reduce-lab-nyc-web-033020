58438956a3b57689230c1ccbea7c3e2b5da1e622
Coral Fussman <coralfussman@gmail.com>
2020-03-23 21:29:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,14 @@
 # Your Code Here
+
+def map(source_array,n)
+    new = []
+    i = 0
+    while i < source_array.length do
+      new.push(yield(array[i] * -1) ) # <== Unique work
+      i += 1
+    end
+   new
+  end
+map 
+
+map([1, 2, 3, -9]){|n| n * n}
