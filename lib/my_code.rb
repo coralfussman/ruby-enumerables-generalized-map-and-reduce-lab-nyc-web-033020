0b4f13b0a9b037597b376f14599835ac081e5c45
Coral Fussman <coralfussman@gmail.com>
2020-03-23 21:30:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,14 +1,12 @@
 # Your Code Here
 
-def map(source_array,n)
+def map(array)
     new = []
     i = 0
     while i < source_array.length do
-      new.push(yield(array[i] * -1) ) # <== Unique work
+      new.push(yield(array[i] * -1) )
       i += 1
     end
    new
   end
 map
-
-map([1, 2, 3, -9]){|n| n * n}
